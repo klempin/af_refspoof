b777907ab212c10c1e7c41d115b0de8237c5d59e
Philip Klempin <philip@klempin.se>
2021-03-13 21:40:52 +0100

    Use correct default value for implode

--- init.php
+++ init.php
@@ -53,8 +53,8 @@ EOF;
 
         $title = __("Fake referral");
         $header = __("Enable referral spoofing based on the feed domain (enter one domain per line)");
-        $enabledDomains = implode("\n", $this->host->get($this, STORAGE_ENABLED_DOMAINS, ""));
         $button = __("Save");
+        $enabledDomains = implode("\n", $this->host->get($this, STORAGE_ENABLED_DOMAINS, array()));
 
         echo <<<EOT
 <div data-dojo-type="dijit/layout/ContentPane" title="<i class='material-icons'>image</i> {$title}"
